3affccdee400855fe916b05c7615edc34f4c4651
arsenal <strigunovqa@gmail.com>
2018-05-07 10:12:31 -0700

    configured acceptance test suite, added Maven Surefire

--- src/main/java/Tests/Acceptance.java
+++ src/main/java/Tests/Acceptance.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 public class Acceptance extends BaseTest {
 
     // 1 - Verify that 'Overview' screen opens when 'Done' button gets pushed;
-    @Test(groups = "Acceptance")
+    @Test(groups = "acceptance")
     public void clickDoneVerifyOverviewScreenOpens() {
         CustomizeScreen customizeScreen = new CustomizeScreen();
         OverviewScreen overviewScreen =  customizeScreen.clickDone();
@@ -20,7 +20,7 @@ public class Acceptance extends BaseTest {
     }
 
     // 2 - Verify that 'Customize' screen opens when 'change' button gets pushed;
-    @Test(groups = "Acceptance")
+    @Test(groups = "acceptance")
     public void clickChangeVerifyCustomizeScreenOpens() {
         OverviewScreen overviewScreen = new CustomizeScreen().clickDone();
         CustomizeScreen customizeScreen = overviewScreen.clickChange();
@@ -28,7 +28,7 @@ public class Acceptance extends BaseTest {
     }
 
     // 3 - Verify that 'Info' screen opens when 'Info' button gets pushed;
-    @Test(groups = "Acceptance")
+    @Test(groups = "acceptance")
     public void clickInfoVerifyInfoScreenOpens() {
         OverviewScreen overviewScreen = new CustomizeScreen().clickDone();
         InfoScreen infoScreen = overviewScreen.clickInfo();
@@ -38,14 +38,15 @@ public class Acceptance extends BaseTest {
 
     // 4 - Verify that when user doesn't customize name and just taps 'done'
     // default name value "Bingles Jingleheimer" is displayed on the 'Overview' screen of the app;
-    @Test(groups = "Acceptance")
+    @Test(groups = "acceptance")
     public void defaultNameIsDisplayed() {
         OverviewScreen overviewScreen = new CustomizeScreen().clickDone();
         Assert.assertTrue(overviewScreen.getNameText().equals("Bingles Jingleheimer"));
     }
 
-    //Verify that after customizing monster's name and tapping 'Done' the chosen name
+    // 5 - Verify that after customizing monster's name and tapping 'Done' the chosen name
     // is displayed on the 'Overview' screen of the app;
+    // DataProvider added for diversification of input
 
     @DataProvider(name = "monsterNamesProvider")
     public Object[][] getMonsterName() {
@@ -55,7 +56,7 @@ public class Acceptance extends BaseTest {
         };
     }
 
-    @Test (dataProvider = "monsterNamesProvider", groups = "Acceptance")
+    @Test (dataProvider = "monsterNamesProvider", groups = "acceptance")
     public void changedNamePassedToOverviewScreen(String[] monsterName) {
         String name = monsterName[0];
         CustomizeScreen customizeScreen = new CustomizeScreen();
@@ -65,7 +66,7 @@ public class Acceptance extends BaseTest {
     }
 
     //Verify that monster name appears in monster's bio on 'Overview' screen;
-    @Test(groups = "Acceptance")
+    @Test(groups = "acceptance")
     public void nameIsDisplayedInBio() {
         String name = "Nick Cage";
         CustomizeScreen customizeScreen = new CustomizeScreen();
@@ -75,7 +76,7 @@ public class Acceptance extends BaseTest {
     }
 
     //Verify that Android share menu rolls up when 'share' button on the 'overview' screen gets pushed
-    @Test(groups = "Acceptance")
+    @Test(groups = "acceptance")
     public void shareMenuDisplayed() {
         OverviewScreen overviewScreen = new CustomizeScreen().clickDone();
         AndroidShareScreen androidShareScreen = overviewScreen.clickShareButton();
